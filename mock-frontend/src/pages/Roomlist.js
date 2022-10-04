576d9a229d84bdc4aa32a33475c7dfc92701e8b6
Marianna Ojanen <marianna.ojanen@kotiposti.net>
2022-10-04 09:05:26 +0300

    Update Roomlist.js

--- mock-frontend/src/pages/Roomlist.js
+++ mock-frontend/src/pages/Roomlist.js
@@ -8,28 +8,13 @@ import { Link } from "react-router-dom";
 const Roomlist = () => {
 
     const [rooms, setRooms] = useState([]);
-    const [showAll, setShowAll] = useState(true);
 
     useEffect(() => {
       getRooms().then((res) => {
         setRooms(res);
       });
     }, []);
- 
-    /*
-    const toggleReservedRoom = (id) => {
-      const room = rooms.find((n) => n.id === id);
-      const changedroom = { ...room, available: !room.available };
-  
-      updateRoom(id, changedroom).then((res) => {
-        setRooms(rooms.map((room) => (room.id === id ? changedroom : room)));
-      });
-    };
-    */
-  
-    const roomsToShow = rooms.filter((room) => room.available === true);
-    const roomsNotAvailable = rooms.filter((room) => room.available === false);
- 
+
     return(
       <div className="container">
          <Table striped>
@@ -40,16 +25,9 @@ const Roomlist = () => {
             </tr>
           </thead>
           <tbody>
-            {roomsToShow.map((room) => (
-              <tr class="table-success">
-                <td>{room.id}</td>
-                <td>{room.size}</td>
-                <td><Link to="/roominfo" className="btn btn-primary btn">Huoneen tiedot</Link></td>
-              </tr>
-            ))}
-            {roomsNotAvailable.map((room) => (
-              <tr class="table-danger">
-                <td>{room.id}</td>
+            {rooms.map((room) => (
+              <tr key={room.id}>
+                <td>{room.name}</td>
                 <td>{room.size}</td>
                 <td><Link to="/roominfo" className="btn btn-primary btn">Huoneen tiedot</Link></td>
               </tr>
