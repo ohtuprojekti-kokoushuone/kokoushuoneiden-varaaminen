1a6b4d428caf589ef77d0907a70867f4e4bf4953
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-10-06 17:49:22 +0300

    frontend eslint configuration

--- mock-frontend/src/types/common.ts
+++ mock-frontend/src/types/common.ts
@@ -1,34 +1,36 @@
 export type Person = {
-    email: string,
-    name: string
-}
+  email: string;
+  name: string;
+};
 
 export type ReservationObject = {
-    subject: string,
-    start: Date,
-    end: Date,
-    attendees?: Person[]
-}
+  subject: string;
+  start: Date;
+  end: Date;
+  attendees?: Person[];
+};
 
 export type ReservationResponse = {
-    id: string,
-    subject: string,
-    start: {
-        dateTime: Date | string,
-        timeZone: string
-    },
-    end: string | {
-        dateTime: Date | string,
-        timeZone: string
-    },
-    organizer: Person,
-    attendees: Person[],
-    isCancelled: boolean
-}
+  id: string;
+  subject: string;
+  start: {
+    dateTime: Date | string;
+    timeZone: string;
+  };
+  end:
+    | string
+    | {
+        dateTime: Date | string;
+        timeZone: string;
+      };
+  organizer: Person;
+  attendees: Person[];
+  isCancelled: boolean;
+};
 
 export type GetReservationsResponse = {
-    count: number,
-    reservations: ReservationResponse[]
-}
+  count: number;
+  reservations: ReservationResponse[];
+};
 
-export type Room = 'testirakennus.2001' | 'testirakennus.2002'
+export type Room = 'testirakennus.2001' | 'testirakennus.2002';
