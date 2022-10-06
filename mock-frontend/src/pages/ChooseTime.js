fd1c50fbf0113633d5cb7d29558c2fccb60c84e4
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-10-06 10:36:17 +0300

    better errors + getting rooms

--- mock-frontend/src/pages/ChooseTime.js
+++ mock-frontend/src/pages/ChooseTime.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Link } from "react-router-dom";
+import { checkAvailability, getRooms } from "../requests";
 
 const ChooseTime = () => {
   
+  const [rooms, setRooms] = useState([]);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   
+  useEffect(()=>{
+    getRooms().then(res => setRooms(res))
+  }, [])
 
   return (
     <div className="container text-center">
@@ -45,7 +50,10 @@ const ChooseTime = () => {
         </div>
 
         <div className="col align-self-center">
-          <Link to="/roomlist" className="btn btn-primary btn-lg">Näytä vapaat kokoushuoneet</Link>
+          
+        <button onClick={()=>rooms.map((room)=>checkAvailability(room.id, startDate, endDate).then(res=>{
+          console.log(res)
+        }))} className="btn btn-primary btn-lg">Näytä vapaat kokoushuoneet</button>
         </div>   
     </div>
   )
