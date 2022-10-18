fd65b1af4924f1b6fd98ba6584901f07fdeb770b
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-10-18 15:47:18 +0300

    no warnings + linting

--- reservatorapp_projekti/ReservatorApp/src/routes/users.js
+++ reservatorapp_projekti/ReservatorApp/src/routes/users.js
@@ -1,12 +1,11 @@
 // Copyright (c) Niko Mäkitalo
 // Licensed under the MIT License.
 
-
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
