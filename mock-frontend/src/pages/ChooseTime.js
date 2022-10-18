6398539bffc5982a0930c314138aa1050497bfd2
Kalle Lindholm <kalle.lindholm@helsinki.fi>
2022-10-18 04:45:58 +0300

    Filters

--- mock-frontend/src/pages/ChooseTime.js
+++ mock-frontend/src/pages/ChooseTime.js
@@ -4,6 +4,7 @@ import fi from 'date-fns/locale/fi';
 import 'react-datepicker/dist/react-datepicker.css';
 import { checkAvailability, getRooms } from '../requests';
 import { Navigate } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 
 registerLocale('fi', fi);
 
@@ -20,7 +21,7 @@ const ChooseTime = ({ user }) => {
   }, []);
 
   return (
-    <div className="container text-center">
+    <Container>
       <div>
         <button className="btn-dark text-white p-1 px-2 mx-4 btn fw-bold mb-2">Exactum</button>
         <button className="btn-dark text-white p-1 px-2 mx-4 btn fw-bold mb-2">Physicum</button>
@@ -48,24 +49,6 @@ const ChooseTime = ({ user }) => {
         timeCaption="Aika"
         locale="fi"
       />
-      <h5>Rajaa hakua</h5>
-      <div className="row justify-content-center">
-        <select className="form-select w-auto justify-content-center">
-          <option defaultValue>Rakennus</option>
-          <option value="Exactum">Exactum</option>
-          <option value="Physicum">Physicum</option>
-          <option value="Chemicum">Chemicum</option>
-        </select>
-      </div>
-      <div className="row justify-content-center">
-        <select className="form-select w-auto justify-content-center">
-          <option defaultValue>Huoneen koko</option>
-          <option value="3">3</option>
-          <option value="6">6</option>
-          <option value="10">10</option>
-          <option value="12">12</option>
-        </select>
-      </div>
 
       <div className="col align-self-center">
         <button
@@ -81,7 +64,7 @@ const ChooseTime = ({ user }) => {
           Näytä vapaat kokoushuoneet
         </button>
       </div>
-    </div>
+    </Container>
   );
 };
 
