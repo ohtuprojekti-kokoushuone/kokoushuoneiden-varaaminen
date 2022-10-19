37fb9e44f9ff6b380687532c4f4914ddfad851c2
Jami Lehtinen <jami.lehtinen1@gmail.com>
2022-10-19 09:30:09 +0300

    Building filtering done. Added mock rooms to mock-backend.

--- mock-frontend/src/pages/Filter.js
+++ mock-frontend/src/pages/Filter.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const Filter = () => {
+  const [filterList, setFilter] = useState([]);
+
+  function toggleFilter(el, name) {
+    el.classList.toggle('filter-selected');
+
+    if (filterList.includes(name.toLowerCase())) {
+      const index = filterList.indexOf(name.toLowerCase());
+      if (index > -1) {
+        filterList.splice(index, 1);
+      }
+    } else {
+      filterList.push(name.toLowerCase());
+    }
+
+    setFilter(filterList);
+    filterRooms();
+  }
+
+  const filterRooms = () => {
+    let cardList = document.getElementsByClassName('card');
+    for (const card of cardList) {
+      card.classList.remove('hidden');
+    }
+
+    if (filterList.length === 0) {
+      return;
+    }
+
+    for (const card of cardList) {
+      if (!filterList.includes(card.getAttribute('data-name').toLowerCase())) {
+        card.classList.add('hidden');
+      }
+    }
+  };
+
   return (
     <div>
-      <button className="btn-dark text-white p-1 px-2 mx-4 btn fw-bold mb-2">Exactum</button>
-      <button className="btn-dark text-white p-1 px-2 mx-4 btn fw-bold mb-2">Physicum</button>
-      <button className="btn-dark text-white p-1 px-2 mx-4 btn fw-bold mb-2">Chemicum</button>
+      <Button
+        variant="dark"
+        className="filter fw-bold mb-2 mx-2"
+        onClick={(el) => toggleFilter(el.target, 'testirakennus')}
+      >
+        Testirakennus
+      </Button>
+      <Button variant="dark" className="filter fw-bold mb-2 mx-2" onClick={(el) => toggleFilter(el.target, 'Exactum')}>
+        Exactum
+      </Button>
+      <Button variant="dark" className="filter fw-bold mb-2 mx-2" onClick={(el) => toggleFilter(el.target, 'Physicum')}>
+        Physicum
+      </Button>
+      <Button variant="dark" className="filter fw-bold mb-2 mx-2" onClick={(el) => toggleFilter(el.target, 'Chemicum')}>
+        Chemicum
+      </Button>
       <Link to="/choosetime" className="btn btn-primary btn-sm mb-2">
         Rajaa tarkemmin
       </Link>
     </div>
   );
 };
+
 export default Filter;
