e37b20f18b6c9b994d99c969d6fde080bc28d6ab
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-10-24 20:44:38 +0300

    Fixed eslint

--- mock-frontend/src/components/RoomCard.test.js
+++ mock-frontend/src/components/RoomCard.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import RoomCard from './RoomCard';
-import { useNavigate, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('<RoomCard />', () => {
   let container;
