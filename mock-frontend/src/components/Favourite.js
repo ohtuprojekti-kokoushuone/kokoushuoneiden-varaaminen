360cce9e0d0fbb6873c47500bc09575ca878eada
annalumi <veera.lumioksa@gmail.com>
2022-11-03 13:52:33 +0200

    Added favourite button

---
+++ mock-frontend/src/components/Favourite.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+
+const fontStyles = { color: 'red', fontSize: '25px', borderRadius: '5px', padding: '1px 1px' };
+
+const Favourite = ({ room }) => {
+  return <SetStateAndToggle room={room} />;
+};
+
+const SetStateAndToggle = ({ room }) => {
+  console.log(room);
+  const [favourite, setFavourite] = useState(false);
+  console.log('favourite on ' + favourite);
+
+  const isNotFavourite = <FaRegHeart style={fontStyles} />;
+  const isFavourite = <FaHeart style={fontStyles} />;
+
+  const toggleFavourite = () => {
+    setFavourite((favourite) => {
+      if (favourite === true) {
+        console.log('Poistettu suosikeista');
+      }
+      if (favourite === false) {
+        console.log('Lisätty suosikkeihin');
+      }
+
+      return !favourite;
+    });
+  };
+
+  return (
+    <button className={fontStyles} onClick={toggleFavourite}>
+      {favourite === true ? isFavourite : isNotFavourite}
+    </button>
+  );
+};
+
+export default Favourite;
