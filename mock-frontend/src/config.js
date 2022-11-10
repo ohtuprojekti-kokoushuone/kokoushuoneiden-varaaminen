041a3e2965083e61feacac3224ca6748e5c5441a
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-11-10 09:10:49 +0200

    config

---
+++ mock-frontend/src/config.js
@@ -0,0 +1,3 @@
+export const baseUrl =
+  // eslint-disable-next-line no-undef
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BASE_URL : process.env.REACT_APP_BASE_URL_DEV;
--- mock-frontend/src/requests.ts
+++ mock-frontend/src/requests.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { ReservationObject, Room } from './types/common';
+import { baseUrl } from './config';
 
 export function setToken(newToken: any) {
   const token = `bearer ${newToken}`;
@@ -7,27 +8,27 @@ export function setToken(newToken: any) {
 }
 
 export async function login(credentials: any) {
-  const res = await axios.post('http://localhost:3003/api/login', credentials);
+  const res = await axios.post(`${baseUrl}/login`, credentials);
   return res.data;
 }
 
 export function getRooms() {
-  const req = axios.get('http://localhost:3003/api/rooms');
+  const req = axios.get(`${baseUrl}/rooms`);
   return req.then((res) => res.data);
 }
 
 export function getRoomsInfo() {
-  const req = axios.get('http://localhost:3003/api/rooms/info');
+  const req = axios.get(`${baseUrl}/rooms/info`);
   return req.then((res) => res.data);
 }
 
 export function getReservations(room: Room, today = false) {
-  const req = axios.get(`http://localhost:3003/api/reservations/${room}?today=${today}`);
+  const req = axios.get(`${baseUrl}/reservations/${room}?today=${today}`);
   return req.then((res) => res.data);
 }
 
 export function makeReservation(room: Room, reservation: ReservationObject) {
-  const req = axios.post(`http://localhost:3003/api/reservations/${room}`, reservation);
+  const req = axios.post(`${baseUrl}/reservations/${room}`, reservation);
   return req
     .then((res) => res.data)
     .catch((error) => {
@@ -36,24 +37,24 @@ export function makeReservation(room: Room, reservation: ReservationObject) {
 }
 
 export function deleteReservation(room: Room, reservationId: string) {
-  const req = axios.delete(`http://localhost:3003/api/reservations/${room}/${reservationId}`);
+  const req = axios.delete(`${baseUrl}/reservations/${room}/${reservationId}`);
   return req.then((res) => res.data);
 }
 
 export function updateReservation(room: Room, reservationId: string, updatedObj: Record<any, any>) {
-  const req = axios.patch(`http://localhost:3003/api/reservations/${room}/${reservationId}`, updatedObj);
+  const req = axios.patch(`${baseUrl}/reservations/${room}/${reservationId}`, updatedObj);
   return req.then((res) => res.data);
 }
 
 export function checkAvailability(room: Room, start: Date, end: Date) {
-  const req = axios.post(`http://localhost:3003/api/reservations/${room}/availability`, { start: start, end: end });
+  const req = axios.post(`${baseUrl}/reservations/${room}/availability`, { start: start, end: end });
   return req.then((res) => {
     return res.data;
   });
 }
 
 export function getRoomById(id: Room) {
-  const req = axios.get(`http://localhost:3003/api/rooms/${id}`);
+  const req = axios.get(`${baseUrl}/rooms/${id}`);
   return req.then((res) => {
     return res.data;
   });
