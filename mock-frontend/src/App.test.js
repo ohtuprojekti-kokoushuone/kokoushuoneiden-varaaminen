6eff3a209698ec0442adf7ae15b24e5cafb0c9e9
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-11-24 16:18:31 +0200

    Fixed App.test.js

--- mock-frontend/src/App.test.js
+++ mock-frontend/src/App.test.js
@@ -2,9 +2,24 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('App component', () => {
-  it('renders', () => {
+  test('renders', async () => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          available: true,
+          building: 'Testirakennus',
+          groups: [],
+          id: 'testirakennus.2001',
+          name: 'Testirakennus, 2001, Kokoushuone 1',
+          size: null
+        }
+      ]
+    });
     expect(() => render(<App />)).not.toThrow();
   });
 });
