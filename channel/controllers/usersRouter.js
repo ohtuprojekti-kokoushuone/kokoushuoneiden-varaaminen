0cebd9f8835a552ca69cafc9ac4f250d468eae03
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2022-12-01 10:23:36 +0200

    current user endpoint

--- channel/controllers/usersRouter.js
+++ channel/controllers/usersRouter.js
@@ -13,4 +13,21 @@ usersRouter.get('/reservations', async (req, res, next) => {
   }
 });
 
+usersRouter.get('/current', async (req, res, next) => {
+  if (!req.headers.mail) {
+    res.status(401).end(JSON.stringify('Unauthorised!!!!!'));
+  }
+  try {
+    const data = {
+      uid: req.headers.uid,
+      mail: req.headers.mail,
+      givenName: req.headers.givenName,
+      sn: req.headers.sn,
+    };
+    res.status(200).end(JSON.stringify(data));
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = usersRouter;
